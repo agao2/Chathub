9c64ca18d838e6019fba0d3709bb0a19a575a557
Alex <18474012+agao2@users.noreply.github.com>
2019-02-25 00:13:11 -0700

    fix styling

--- web_app/src/components/Chatroom/Chatroom.js
+++ web_app/src/components/Chatroom/Chatroom.js
@@ -12,27 +12,18 @@ class Chatroom extends Component {
                 {
                     text: "This is a test message!",
                     member: {
+                        id:1,
                         color: "blue",
                         username: "bluemoon"
                     }
                 }
             ],
             member: {
-                username: this.randomName(),
-                color: this.randomColor()
+                id:2,
+                username: "Computer",
+                color: "Green"
             }
         }
-    }
-    randomName = () => {
-        const adjectives = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
-        const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
-        const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-        const noun = nouns[Math.floor(Math.random() * nouns.length)];
-        return adjective + noun;
-    }
-
-    randomColor = () => {
-        return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
     }
 
     onSendMessage = (message) => {
