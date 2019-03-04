9a2ef8d133d544e8c9002af0f3ce750c7a3eaf2a
Alex <18474012+agao2@users.noreply.github.com>
2019-03-04 00:39:26 -0700

    Add navbar

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Chatroom from './components/Chatroom'
+import Navbar from './components/Navbar'
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
-          <Chatroom></Chatroom>
+        <Navbar></Navbar>
+        <Chatroom></Chatroom>
       </div>
     );
   }
