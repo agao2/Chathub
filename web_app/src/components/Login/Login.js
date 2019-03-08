8516ec929b15418fcd41b00a9f8a1a520c6e3727
Alex Gao <agao@ep.com>
2019-03-08 14:42:15 -0700

    Update login to track input text

--- web_app/src/components/Login/Login.js
+++ web_app/src/components/Login/Login.js
@@ -3,16 +3,34 @@ import './Login.css'
 
 class Login extends Component {
 
-    render() {
-        return (
-            <div className="box" >
-              <h1>login</h1>
-              <input type="text" name="username" id="username" placeholder="Username" autoComplete="off" />
-              <input type="password" name="pass" id="pass" placeholder="Password" autoComplete="off" />
-              <input type="submit" id="submit" defaultValue="login" />
-            </div>
-          );
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
     }
+  }
+
+  onChange = (event) => {    
+    let state = this.state;
+    state[event.target.id] = event.target.value;
+  }
+
+  onSubmit = () => {
+    console.log(this.state.username);
+    console.log(this.state.password);
+  }
+
+  render() {
+    return (
+      <div className="box" >
+        <h1>login</h1>
+        <input type="text" name="username" id="username" placeholder="Username" autoComplete="off" defaultValue={this.state.username} onChange={this.onChange} />
+        <input type="password" name="password" id="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password} onChange={this.onChange} />
+        <input type="submit" id="submit" defaultValue="login" onClick={this.onSubmit} />
+      </div>
+    );
+  }
 
 
 }
