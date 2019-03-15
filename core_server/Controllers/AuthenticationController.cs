1c7b7614de4edfedf02fcb871132b676ba8de7aa
Alex Gao <agao@ep.com>
2019-03-15 15:59:59 -0700

    Authentication API setup

---
+++ core_server/Controllers/AuthenticationController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using core_server.Models;
+
+
+namespace core_server.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+
+        private readonly UserContext _context;
+        public AuthenticationController(UserContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult<UserDTO> Post(Authentication authentication)
+        {
+            User user = _context.Users.Where(u => u.Username == authentication.Username).SingleOrDefault();
+
+            if (user == null)
+                return NotFound("User does not exist");
+
+            if (!user.Password.Equals(authentication.Password))
+                return Forbid("Password does not match");
+
+            return new UserDTO
+            {
+                Username = user.Username,
+                EmailAddress = user.EmailAddress,
+                Password = user.Password,
+                DateCreated = user.DateCreated
+            };
+
+        }
+
+    }
+}
---
+++ core_server/Models/Authentication.cs
@@ -0,0 +1,11 @@
+namespace core_server.Models
+{
+    public class Authentication
+    {
+        public string Username { get; set; }
+   
+        public string EmailAddress { get; set; }
+
+        public string Password { get; set; }
+    }
+}
