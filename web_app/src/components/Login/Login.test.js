7523ff970b16cc202f1bc801d36fe6185371d074
Alex Gao <agao@ep.com>
2019-04-25 10:59:27 -0700

    Verbose logging and more unit tests

--- web_app/src/components/Login/Login.test.js
+++ web_app/src/components/Login/Login.test.js
@@ -1,28 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login';
-import { shallow , configure} from "enzyme";
+import { shallow, configure } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 const mockProps = {
-    authenticate:jest.fn()
+    authenticate: jest.fn()
 }
 
-describe("Login Component" , () => {
+describe("Login Component", () => {
+
     it('Login component renders without crashing', () => {
         const div = document.createElement('div');
         ReactDOM.render(<Login />, div);
         ReactDOM.unmountComponentAtNode(div);
     });
-    
+
     it('onSubmit is called when login button is clicked ', () => {
         const wrapper = shallow(<Login {...mockProps} />);
-        let onSubmit = jest.spyOn(wrapper.instance(), 'onSubmit')
+        const onSubmit = jest.spyOn(wrapper.instance(), 'onSubmit')
         wrapper.instance().forceUpdate();
         wrapper.find('#submit').prop('onClick')();
         expect(onSubmit).toHaveBeenCalled();
+    })
+
+    it('Editting username inputs should set username', () => {
+        const wrapper = shallow(<Login />)
+        wrapper.find('#username').simulate("change", { target: { value: "alex", id: "username" } });
+        expect(wrapper.state().username).toEqual("alex");
+    })
+
+    it('Editing password input should set password', () => {
+        const wrapper = shallow(<Login />)
+        wrapper.find("#password").simulate("change", { target: { value: "password", id: "password" } });
+        expect(wrapper.state().password).toEqual("password");
+    })
+
+    it('Login button makes authenticate api call', () => {
+        const wrapper = shallow(<Login {...mockProps} />);
+        wrapper.find('#submit').prop('onClick')();
+        expect(mockProps.authenticate).toHaveBeenCalled();
     })
 })
 
