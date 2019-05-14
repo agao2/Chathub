97c6e27803e857505add2dab228449596ef097c5
Alex Gao <agao@ep.com>
2019-05-14 13:27:05 -0700

    Very rough grid

--- web_app/src/components/CardWrapper/CardWrapper.js
+++ web_app/src/components/CardWrapper/CardWrapper.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import './CardWrapper.css'
 
 
 const CardWrapper = (props) => {
     return (
-        <Card >
+        <Card className="CardWrapper">
             <CardContent>
-                <Typography color="textSecondary" gutterBottom>
-                    Word of the Day
-        </Typography>
-
+                <Typography component="h4" variant="h4">
+                    {props.title || "Default Title"}
+                </Typography>
+                <Typography variant="subtitle1" color="textSecondary">
+                    {props.subtitle || "Default SubTitle"}
+                </Typography>
             </CardContent>
-            <CardActions>
-                <Button size="small">Learn More</Button>
-            </CardActions>
         </Card>
     );
 }
 
-// Card.propTypes = {
-//   classes: PropTypes.object.isRequired,
-// };
+Card.propTypes = {
+    Title: PropTypes.object.isRequired,
+    SubTitle: PropTypes.object.isRequired,
+};
 
 export default CardWrapper;
--- web_app/src/components/Chathub/Chathub.js
+++ web_app/src/components/Chathub/Chathub.js
@@ -1,11 +1,42 @@
 import React, { Component } from 'react';
 import CardWrapper from '../CardWrapper'
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 class Chathub extends Component {
+
+
+
+    FormRow = (props) => {
+        return (
+            <React.Fragment>
+                <Grid item xs={4}>
+                    <CardWrapper></CardWrapper>
+                </Grid>
+                <Grid item xs={4}>
+                    <CardWrapper></CardWrapper>
+                </Grid>
+                <Grid item xs={4}>
+                    <CardWrapper></CardWrapper>
+                </Grid>
+            </React.Fragment>
+        );
+    }
+
 
     render = () => {
         return (
-            <div>
-                <CardWrapper />
+            <div >
+                <Grid container spacing={2}>
+                    <Grid container item xs={12} spacing={24}>
+                        {this.FormRow()}
+                    </Grid>
+                    <Grid container item xs={12} spacing={24}>
+                        {this.FormRow()}
+                    </Grid>
+                    <Grid container item xs={12} spacing={24}>
+                        {this.FormRow()}
+                    </Grid>
+                </Grid>
             </div>
         )
     }
