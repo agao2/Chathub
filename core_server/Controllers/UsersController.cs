e0e11f3406c76fd53127b0e4c29a6dee7addafbf
Alex Gao <agao@ep.com>
2019-05-17 15:20:30 -0700

    Convert user controller stuff to async

--- core_server/Controllers/UsersController.cs
+++ core_server/Controllers/UsersController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using core_server.Models;
-
+using Microsoft.EntityFrameworkCore;
 
 namespace core_server.Controllers
 {
@@ -23,16 +23,16 @@ namespace core_server.Controllers
 
         //api/Users
         [HttpGet]
-        public ActionResult<IEnumerable<User>> Get()
+        public async Task<ActionResult<IEnumerable<User>>> Get()
         {
-            return _context.Users.ToArray();
+            return await _context.Users.ToListAsync();
         }
 
         //api/Users/2
         [HttpGet("{id}")]
-        public ActionResult<User> Get(int id)
+        public async Task<ActionResult<User>> Get(int id)
         {
-            User user = _context.Users.Where(u => u.ID == id).SingleOrDefault();
+            User user = await _context.Users.Where(u => u.ID == id).SingleOrDefaultAsync();
 
             if (user == null)
                 return StatusCode(404, "User not found");
@@ -42,25 +42,27 @@ namespace core_server.Controllers
 
         //api/Users
         [HttpPost]
-        public ActionResult<UserDTO> Post(UserDTO userDTO)
+        public async Task<ActionResult<UserDTO>> Post(UserDTO userDTO)
         {
-            _context.Users.Add(new User
-            {
-                Username = userDTO.Username,
-                EmailAddress = userDTO.EmailAddress,
-                Password = userDTO.Password,
-                DateCreated = DateTime.Now.ToString(),
-                PasswordSalt = "salt"
-            });
+            await _context.Users.AddAsync(
+                new User
+                {
+                    Username = userDTO.Username,
+                    EmailAddress = userDTO.EmailAddress,
+                    Password = userDTO.Password,
+                    DateCreated = DateTime.Now.ToString(),
+                    PasswordSalt = "salt"
+                }
+            );
             _context.SaveChanges();
             return userDTO;
         }
 
         //api/Users
         [HttpDelete("{id}")]
-        public ActionResult Delete(int id)
+        public async Task<ActionResult> Delete(int id)
         {
-            User user = _context.Users.Where(u => u.ID == id).SingleOrDefault();
+            User user = await _context.Users.Where(u => u.ID == id).SingleOrDefaultAsync();
 
             if (user == null)
                 return StatusCode(404, "User not found");
