e739e57377cccaaf03f3c2f17ac748c5b90dac92
Alex Gao <agao@ep.com>
2019-05-20 16:04:13 -0700

    Convert cardwrapper from functional component to full component

--- web_app/src/components/CardWrapper/CardWrapper.js
+++ web_app/src/components/CardWrapper/CardWrapper.js
@@ -1,34 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import ButtonBase from "@material-ui/core/ButtonBase";
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+// import ButtonBase from "@material-ui/core/ButtonBase";
+// import Link from "@material-ui/core/Link"
 import './CardWrapper.css'
 
 
-const CardWrapper = (props) => {
-    return (
-      
-            <Card className="CardWrapper">
-                <CardContent className="CardContent">
-                    <h4>
-                        {props.title || "Default Title"}
-                    </h4>
-                    <p>
-                        {props.subtitle || "Default Subtitle"}
-                    </p>
-                </CardContent>
-            </Card>
+class CardWrapper extends Component {
     
-    );
+    render =  () => {
+        return (
+            <div>
+                <Card className="CardWrapper">
+                    <CardContent className="CardContent">
+                        <h4>
+                            {this.props.title || "Default Title"}
+                        </h4>
+                        <p>
+                            {this.props.subtitle || "Default Subtitle"}
+                        </p>
+                    </CardContent>
+                </Card>
+                </div>
+        );
+    }
 }
 
-Card.propTypes = {
-    Title: PropTypes.object.isRequired,
-    SubTitle: PropTypes.object.isRequired,
-};
+// Card.propTypes = {
+//     Title: PropTypes.object.isRequired,
+//     SubTitle: PropTypes.object.isRequired,
+// };
 
 export default CardWrapper;
