e096bb8c6e4e5f66d1aaa4f86ff4255c8778393a
Alex Gao <agao@ep.com>
2019-05-21 14:52:16 -0700

    Copy someone elses jwt and pwd hashing code

---
+++ core_server/Security/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace core_server.Security
+{
+    public interface IJwtTokenGenerator
+    {
+        Task<string> CreateToken(string username);
+    }
+}
---
+++ core_server/Security/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace core_server.Security
+{
+    public interface IPasswordHasher
+    {
+        byte[] Hash(string password, byte[] salt);
+    }
+}
