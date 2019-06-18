592c80b58033877b31080cd338037c99cec3a36b
agao2 <agao2@asu.edu>
2019-06-18 12:20:52 -0700

    Initial conversion from enzyme to react testing library

--- web_app/src/components/Login/Login.test.js
+++ web_app/src/components/Login/Login.test.js
@@ -1,47 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Login from './Login';
-import { shallow, configure } from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
+import {render, fireEvent, cleanup, waitForElement} from '@testing-library/react'
+import axiosMock from 'axios'
 
-configure({ adapter: new Adapter() });
 
-const mockProps = {
-    authenticate: jest.fn()
-}
+afterEach(cleanup)
 
-describe("Login Component", () => {
 
-    it('Login component renders without crashing', () => {
-        const div = document.createElement('div');
-        ReactDOM.render(<Login />, div);
-        ReactDOM.unmountComponentAtNode(div);
-    });
+test('Login component renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Login />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
 
-    it('onSubmit is called when login button is clicked ', () => {
-        const wrapper = shallow(<Login {...mockProps} />);
-        const onSubmit = jest.spyOn(wrapper.instance(), 'onSubmit')
-        wrapper.instance().forceUpdate();
-        wrapper.find('#submit').prop('onClick')();
-        expect(onSubmit).toHaveBeenCalled();
-    })
-
-    it('Editting username inputs should set username', () => {
-        const wrapper = shallow(<Login />)
-        wrapper.find('#username').simulate("change", { target: { value: "alex", id: "username" } });
-        expect(wrapper.state().username).toEqual("alex");
-    })
-
-    it('Editing password input should set password', () => {
-        const wrapper = shallow(<Login />)
-        wrapper.find("#password").simulate("change", { target: { value: "password", id: "password" } });
-        expect(wrapper.state().password).toEqual("password");
-    })
-
-    it('Login button makes authenticate api call', () => {
-        const wrapper = shallow(<Login {...mockProps} />);
-        wrapper.find('#submit').prop('onClick')();
-        expect(mockProps.authenticate).toHaveBeenCalled();
-    })
+test('onSubmit is called when login button is clicked ', () => {
+    const mockProps = {
+        authenticate: jest.fn()
+    }
+    const {container} = render(<Login {...mockProps} />);
+    fireEvent.click(container.querySelector("#submit"));
+    expect(mockProps.authenticate).toHaveBeenCalled();
 })
+
+    // it('Editting username inputs should set username', () => {
+    //     const wrapper = shallow(<Login />)
+    //     wrapper.find('#username').simulate("change", { target: { value: "alex", id: "username" } });
+    //     expect(wrapper.state().username).toEqual("alex");
+    // })
+
+    // it('Editing password input should set password', () => {
+    //     const wrapper = shallow(<Login />)
+    //     wrapper.find("#password").simulate("change", { target: { value: "password", id: "password" } });
+    //     expect(wrapper.state().password).toEqual("password");
+    // })
+
+    // it('Login button makes authenticate api call', () => {
+    //     const wrapper = shallow(<Login {...mockProps} />);
+    //     wrapper.find('#submit').prop('onClick')();
+    //     expect(mockProps.authenticate).toHaveBeenCalled();
+    // })
+
 
