fdefca64907acc5a92571cedbd1b612a9374e505
Alex <18474012+agao2@users.noreply.github.com>
2019-09-15 01:55:19 -0700

    Material ui login page

--- web_app/src/components/Login/Login.js
+++ web_app/src/components/Login/Login.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import './Login.css'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
 
 class Login extends Component {
@@ -7,34 +17,99 @@ class Login extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: "",
+      emailAddress: "",
       password: ""
     }
   }
-
   onChange = (event) => {
     let state = this.state;
     state[event.target.id] = event.target.value;
   }
 
-  onSubmit = async () => {
-    await this.props.authenticate({
-      emailAddress:this.state.emailAddress, 
-      password:this.state.password
-    })
-    if(this.props.User){
-      this.props.history.push("/");
+  onSubmit = async (event) => {
+    // this triggers form validation
+    if (event.currentTarget.form.reportValidity()) {
+      await this.props.authenticate({
+        emailAddress: this.state.emailAddress,
+        password: this.state.password
+      })
+      if (this.props.User) {
+        this.props.history.push("/");
+      }
     }
   }
 
   render() {
     return (
-      <div className="box" >
-        <h1>login</h1>
-        <input type="text" name="emailAddress" id="emailAddress" placeholder="emailAddress" autoComplete="off" defaultValue={this.state.emailAddress} onChange={this.onChange} />
-        <input type="password" name="password" id="password" placeholder="Password" autoComplete="off" defaultValue={this.state.password} onChange={this.onChange} />
-        <input type="submit" id="submit" defaultValue="login" onClick={this.onSubmit} />
-      </div>
+      <Container component="main" maxWidth="xs">
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: '20%',
+        }}>
+          <Avatar>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+        </Typography>
+          <form style={{
+            width: '100%', // Fix IE 11 issue.
+            display: 'block'
+          }}>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              required
+              id="emailAddress"
+              label="Email Address"
+              name="emailAddress"
+              autoComplete="email"
+              autoFocus
+              onChange={this.onChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              required
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={this.onChange}
+            />
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={this.onSubmit}
+              style={{ margin: '2% 0 2% 0 ' }}
+            >
+              Sign In
+          </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+              </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Container>
     );
   }
 }
